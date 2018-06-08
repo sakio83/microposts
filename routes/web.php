972a32feb1a3c7c591df6e8fb214c173fc3335ff
saki.ota <saki.a.ota@rakuten.com>
2018-06-08 01:55:08 +0000

    Procfile

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get')
 ;
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('users', 'UserController', ['only' => ['index', 'show']]);
 });
 
 Route::get('/', 'MicropostsController@index');
@@ -31,6 +31,6 @@ Route::get('/', 'MicropostsController@index');
 // omit
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('users', 'UserController', ['only' => ['index', 'show']]);
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
